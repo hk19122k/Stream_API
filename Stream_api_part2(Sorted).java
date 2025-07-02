ef741c16b533ccb2b83acfc3e9f5afc162ae89cf
HarisKumarLV <126394823+hk19122k@users.noreply.github.com>
2025-07-02 11:27:46 +0530

    Update and rename Stream_api_part2(Collectors).java to Stream_api_part2(Sorted).java
    Commit

--- Stream_api_part2(Sorted).java
+++ Stream_api_part2(Sorted).java
@@ -68,41 +68,38 @@ public class Onepiece2 {
                 new Employee("Sanji", 68),
                 new Employee("Luffy", 900));
 
-        // ----------------------------one set of sorted and Comparator
-        // -----------------------------------------------------//
+        ----------------------------one set of sorted and Comparator -----------------------------------------------------//
 
-        // e.stream()
+        e.stream()
 
-        // ----------- this is how wwe compare and sort based on employee salary in
-        // ascending irder --------------//
+        ----------- this is how wwe compare and sort based on employee salary in ascending order --------------//
 
-        // .sorted(Comparator.comparing(Employee::getSalary))
+        .sorted(Comparator.comparing(Employee::getSalary))
 
-        // .forEach(f -> System.out.println(f.getName() + ": " + f.getSalary()));
+        .forEach(f -> System.out.println(f.getName() + ": " + f.getSalary()));
 
-        // ----------- this is how wwe compare and sort based on employee salary in
-        // descending order --------------//
+        ----------- this is how wwe compare and sort based on employee salary in descending order --------------//
 
-        // e.stream()
-        // .sorted(Comparator.comparing(Employee::getSalary).reversed()) //to use
-        // reverse
+        e.stream()
+        .sorted(Comparator.comparing(Employee::getSalary).reversed()) //to use
+        reverse
 
-        // .forEach(ff -> System.out.println(ff.getName() + ":" + ff.getSalary()));
+        .forEach(ff -> System.out.println(ff.getName() + ":" + ff.getSalary()));
 
-        // --------- real-world multi-level comparator — extremely important for
-        // backend, dashboards, and data presentation----------------//
+        --------- real-world multi-level comparator — extremely important for backend, dashboards, and data presentation----------------//
 
-        // List<Movie> movies = List.of(
-        // new Movie("One Piece Film: Z", 2012, 8.0),
-        // new Movie("One Piece Film: Red", 2022, 7.5),
-        // new Movie("One Piece Film: Gold", 2016, 7.9));
+        List<Movie> movies = List.of(
+        new Movie("One Piece Film: Z", 2012, 8.0),
+        new Movie("One Piece Film: Red", 2022, 7.5),
+        new Movie("One Piece Film: Gold", 2016, 7.9));
 
-        // movies.stream()
-        // .sorted(Comparator.comparing(Movie::getYear)
-        // .thenComparing(Comparator.comparing(Movie::getRating).reversed())) //joining
-        // 2 compartors to one and then sorting it
-        // .forEach(k -> System.out.println(k.getTitle() + " : " + k.getYear() + " : " +
-        // k.getRating()));
+        movies.stream()
+        .sorted(Comparator.comparing(Movie::getYear)
+        .thenComparing(Comparator.comparing(Movie::getRating).reversed())) //joining 2 compartors to one and then sorting it
+        .forEach(k -> System.out.println(k.getTitle() + " : " + k.getYear() + " : " +
+        k.getRating()));
+
+        
 
         List<Pirate> crew = Arrays.asList(
                 new Pirate("Luffy", 1500000000),
@@ -111,7 +108,7 @@ public class Onepiece2 {
                 new Pirate("Jinbei", 438000000));
 
         crew.stream()
-                .sorted(Comparator.comparingInt((Pirate p) -> p.bounty).reversed())
+                .sorted(Comparator.comparingInt((Pirate p) -> p.bounty).reversed()) /// here we need mention the object name while using lambda explicit type and we dodint use getters here
                 .limit(3)
                 .forEach(System.out::println);
 
