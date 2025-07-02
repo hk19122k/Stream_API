378d6b020c3544622b5134f50dc4e165ae59ea19
HarisKumarLV <126394823+hk19122k@users.noreply.github.com>
2025-07-02 10:49:40 +0530

    Add files via upload
    commit

---
+++ Stream_api_part1.java
@@ -0,0 +1,98 @@
+
+public class onepiece {
+
+    public static void main(String[] args) {
+
+        // List<String> b = Arrays.asList("Zoro", "Sanji", "Buggy", "Luffy");
+
+        // --------------------- STREAM API & Filter() &
+        // forEach()-------------------------//
+
+        // b.stream() ///converts list to stream
+        // .filter(n -> !n.equals("Buggy")) ///this is the lambda function that uses
+        // predicate interface
+        // .forEach(n -> System.out.println(n)); // this l function uses
+        // consumerinterface
+
+        // -------------------- MAP() --------------------------------//
+
+        // b.stream()
+        // .filter(n -> !n.equals("Buggy"))
+        // .map(n -> n + " Haki unlocker") //.map() is used to transform each element in
+        // the stream
+        // .forEach(System.out::println);
+
+        // -------------------------Collect()--------------------------//
+
+        // List<String> a = b.stream()
+        // .filter(n -> n.length() > 4)
+        // .map(n -> n + " mugivara")
+        // .collect(Collectors.toList()); /// collect is used to convert Stream API that
+        // gathers elements from the stream into a data structure
+
+        // System.out.println(a);
+
+        /////---------------------Sorted()------------------------//
+
+        // List<Integer> l = Arrays.asList(15, 32, 9, 500, 98, 8);
+        // l.stream()
+        // .sorted()
+        // .forEach(System.out::println);
+
+        // ---------------------alpabetic sort-------------------------//
+
+        // b.stream()
+        // .sorted(Comparator.comparing(m -> m.toLowerCase())) ///sorted based on
+        // alphabetical order
+        // .forEach(System.out::println);
+
+        // ---------------------Advanced sort concepts-------------------------//
+
+        // List<Integer> li = Arrays.asList(15, 32, 9, 500, 98, 8);
+        // li.stream()
+        // .sorted(Comparator.comparingInt(n -> String.valueOf(n).length())) ///convert
+        // the number value into string and count the digits and retuen based in that .
+        // .forEach(System.out::println);
+
+        // --------------------Reduce() method -------------------------///
+
+        // List<Integer> pp = Arrays.asList(150, 320, 900);
+        // int t = pp.stream()
+        // .reduce(0, (sum, target) -> sum + target); // it will add the list elements
+        // simple
+        // System.out.println(t);
+
+        // ----------------------------Distinct()---------------------------//
+
+        // List<String> pi = Arrays.asList("Luffy", "Zoro", "Zoro", "Nami");
+        // pi.stream()
+        // .distinct()
+        // .forEach(System.out::println);
+
+        // --------------------limit &
+        // skip---------------------------------------------//
+        // List<String> al = Arrays.asList("Luffy", "Zoro", "Sanji", "Nami", "Usopp",
+        // "Chopper");
+        // System.out.println("First 3 Pirates:");
+        // al.stream().limit(3).forEach(System.out::println); //// used to limit only 3
+        // values
+
+        // al.stream().skip(2).forEach(System.out::println); //used to skip first two
+        // values in list
+
+        // --------------------------Match()--------------------------------//
+
+        // List<String> crew = Arrays.asList("Luffy", "Zoro", "Chopper", "Nami");
+        // boolean j = crew.stream()
+        // .anyMatch(n -> n.equals("Nami"));
+        // System.out.println(j);
+
+        // --------------------------Custom Collectors--------------------------------//
+
+        // List<String> cp = Arrays.asList("Luffy", "Zoro", "Sanji");
+        // String r = cp.stream().collect(Collectors.joining(" ! "));
+        // System.out.println(r);
+
+    }
+
+}
---
+++ Stream_api_part2(Collectors).java
@@ -0,0 +1,120 @@
+import java.util.*;
+
+class Employee {
+    private String name;
+    private double salary;
+
+    // Constructor
+    public Employee(String name, double salary) {
+        this.name = name;
+        this.salary = salary;
+    }
+
+    // Getter for name
+    public String getName() {
+        return name;
+    }
+
+    // Getter for salary
+    public double getSalary() {
+        return salary;
+    }
+}
+
+class Movie {
+    private String title;
+    private int year;
+    private double rating;
+
+    // Constructor
+    public Movie(String title, int year, double rating) {
+        this.title = title;
+        this.year = year;
+        this.rating = rating;
+    }
+
+    // Getters
+    public String getTitle() {
+        return title;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public double getRating() {
+        return rating;
+    }
+}
+
+class Pirate {
+    String name;
+    int bounty;
+
+    Pirate(String name, int bounty) {
+        this.name = name;
+        this.bounty = bounty;
+    }
+
+    public String toString() {
+        return name + " → " + bounty;
+    }
+}
+
+public class Onepiece2 {
+    public static void main(String[] args) {
+        List<Employee> e = List.of(
+                new Employee("Zoro", 75),
+                new Employee("Sanji", 68),
+                new Employee("Luffy", 900));
+
+        // ----------------------------one set of sorted and Comparator
+        // -----------------------------------------------------//
+
+        // e.stream()
+
+        // ----------- this is how wwe compare and sort based on employee salary in
+        // ascending irder --------------//
+
+        // .sorted(Comparator.comparing(Employee::getSalary))
+
+        // .forEach(f -> System.out.println(f.getName() + ": " + f.getSalary()));
+
+        // ----------- this is how wwe compare and sort based on employee salary in
+        // descending order --------------//
+
+        // e.stream()
+        // .sorted(Comparator.comparing(Employee::getSalary).reversed()) //to use
+        // reverse
+
+        // .forEach(ff -> System.out.println(ff.getName() + ":" + ff.getSalary()));
+
+        // --------- real-world multi-level comparator — extremely important for
+        // backend, dashboards, and data presentation----------------//
+
+        // List<Movie> movies = List.of(
+        // new Movie("One Piece Film: Z", 2012, 8.0),
+        // new Movie("One Piece Film: Red", 2022, 7.5),
+        // new Movie("One Piece Film: Gold", 2016, 7.9));
+
+        // movies.stream()
+        // .sorted(Comparator.comparing(Movie::getYear)
+        // .thenComparing(Comparator.comparing(Movie::getRating).reversed())) //joining
+        // 2 compartors to one and then sorting it
+        // .forEach(k -> System.out.println(k.getTitle() + " : " + k.getYear() + " : " +
+        // k.getRating()));
+
+        List<Pirate> crew = Arrays.asList(
+                new Pirate("Luffy", 1500000000),
+                new Pirate("Zoro", 320000000),
+                new Pirate("Sanji", 330000000),
+                new Pirate("Jinbei", 438000000));
+
+        crew.stream()
+                .sorted(Comparator.comparingInt((Pirate p) -> p.bounty).reversed())
+                .limit(3)
+                .forEach(System.out::println);
+
+    }
+
+}
